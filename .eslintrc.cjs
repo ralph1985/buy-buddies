2ce6d5ce5e5bd12f43627e1974609f177751953d
ralph1985 <rafaelgarcia1985@hotmail.com>
2025-08-10 00:35:07 +0200

    chore: add ESLint ignore file

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,37 @@
+/** @type {import('eslint').Linter.Config} */
 module.exports = {
-  env: {
-    es2020: true,
-    node: true,
-    browser: true,
+  root: true,
+  env: { es2022: true, node: true, browser: true },
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  plugins: [
+    '@typescript-eslint',
+    'import',
+    'promise',
+    'unicorn',
+    'lit',
+    'lit-a11y',
+    'simple-import-sort'
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:promise/recommended',
+    'plugin:unicorn/recommended',
+    'plugin:lit/recommended',
+    'plugin:lit-a11y/recommended',
+    'prettier'
+  ],
+  settings: { 'import/resolver': { node: { extensions: ['.ts', '.js'] } } },
+  rules: {
+    'unicorn/prefer-module': 'off',
+    'unicorn/filename-case': 'off',
+    'import/no-unresolved': 'off',
+    'import/order': 'off',
+    'simple-import-sort/imports': 'warn',
+    'simple-import-sort/exports': 'warn',
+    '@typescript-eslint/explicit-function-return-type': 'off'
   },
-  extends: ['eslint:recommended'],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  rules: {},
+  ignorePatterns: ['dist', 'node_modules']
 };
