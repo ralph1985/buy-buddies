9832f0442ea085ccf256fa54e97c9cc75d644fc9
ralph1985 <rafaelgarcia1985@hotmail.com>
2025-08-11 21:21:56 +0200

    chore: isolate e2e and unit tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
+  testDir: 'tests/e2e',
+  testMatch: ['**/*.spec.ts'],
   timeout: 30_000,
   retries: 1,
   reporter: [['list']],
